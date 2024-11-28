80199e88d8f32ae5597ebcdb26ed500af5fd5d67
unknown <jiahchengpan@gmail.com>
2024-11-28 19:47:29 +0000

    Mongo lab Error Handling

--- tasky-api/api/tasks/taskModel.js
+++ tasky-api/api/tasks/taskModel.js
@@ -3,13 +3,18 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const TaskSchema = new Schema({
-  title:  String,
-  description:  String ,
-  deadline: Date,
+    title: String,
+    description: String,
+    deadline: Date,
     done: Boolean,
-    priority: {type: String, enum: ["low","medium","high"]},
+    priority: { type: String, enum: ["low", "medium", "high"] },
     created_at: Date,
     updated_at: Date
 });
+
+const dateValidator = (date) => {
+    return date > new Date();
+};
+TaskSchema.path("deadline").validate(dateValidator);
 
 export default mongoose.model('Task', TaskSchema);
